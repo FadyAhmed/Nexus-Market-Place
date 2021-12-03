3c3cf6190d8580f21272cdc9dab43e86e2eac88f
Amr Fatouh <amrfatouh1499@gmail.com>
2021-12-03 19:59:28 +0200

    acount info: get not sold items

--- lib/screens/account_info/account_info/not_sold_items.dart
+++ lib/screens/account_info/account_info/not_sold_items.dart
@@ -1,7 +1,15 @@
 import 'package:ds_market_place/components/UI/item_card.dart';
+import 'package:ds_market_place/components/UI/show_snackbar.dart';
+import 'package:ds_market_place/constants/enums.dart';
+import 'package:ds_market_place/helpers/exceptions.dart';
+import 'package:ds_market_place/helpers/functions.dart';
 import 'package:ds_market_place/models/inventory_item.dart';
+import 'package:ds_market_place/models/store_item.dart';
+import 'package:ds_market_place/providers/stores_provider.dart';
 import 'package:ds_market_place/screens/edit_item_details.dart';
+import 'package:ds_market_place/screens/seller_item_details.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class OnSaleItemsScreen extends StatefulWidget {
   const OnSaleItemsScreen({Key? key}) : super(key: key);
@@ -11,44 +19,78 @@ class OnSaleItemsScreen extends StatefulWidget {
 }
 
 class _OnSaleItemsScreenState extends State<OnSaleItemsScreen> {
+  Future<void> fetchAllItemsFromMyStore() async {
+    try {
+      await Provider.of<StoresProvider>(context, listen: false)
+          .getAllItemsFromMyStore(notifyWhenLoading: false);
+    } on ServerException catch (e) {
+      showMessageDialogue(context, e.message);
+    }
+  }
+
+  void submitDelete(String id) async {
+    try {
+      await Provider.of<StoresProvider>(context, listen: false)
+          .removeItemFromMyStore(id);
+      showSnackbar(context, Text('Item is deleted'));
+    } on ServerException catch (e) {
+      showMessageDialogue(context, e.message);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAllItemsFromMyStore();
+  }
+
   @override
   Widget build(BuildContext context) {
+    var storeProvider = Provider.of<StoresProvider>(context);
+    List<StoreItem>? ownedItems;
+    if (storeProvider.items != null)
+      ownedItems = storeProvider.items!
+          .where((item) => item.state == StoreItemState.owned)
+          .toList();
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 7,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ItemCard(
-            menuItems: ["Edit", "Remove"],
-            itemName: "item name",
-            amount: "11",
-            price: 15,
-            onPressed: null,
-            onSelectMenuItem: (choice) {
-              if (choice == "Edit") {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => EditItemDetails(
-                    inventoryItem: InventoryItem(
-                      name: 'name',
-                      amount: 1,
-                      price: 1,
-                      description: 'description',
-                      imageLink: 'imageLink',
-                    ),
-                    submitButtonText: "Edit",
-                    onSubmit: () => {
-                      //TODO: add edit habdler
-                      Navigator.of(context).pop()
+      body: storeProvider.loadingStatus == LoadingStatus.loading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: ownedItems!.length,
+              itemBuilder: (context, index) {
+                StoreItem item = ownedItems![index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ItemCard(
+                    menuItems: ["Edit", "Remove"],
+                    itemName: item.name,
+                    amount: item.amount.toString(),
+                    price: item.price,
+                    onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                OnSaleItemDetailsScreen(storeItem: item))),
+                    onSelectMenuItem: (choice) {
+                      if (choice == "Edit") {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => EditItemDetails(
+                            storeItem: item,
+                            submitButtonText: "Edit",
+                            onSubmit: () => {
+                              //TODO: add edit habdler
+                              Navigator.of(context).pop()
+                            },
+                          ),
+                        ));
+                      } else {
+                        submitDelete(item.id!);
+                      }
                     },
                   ),
-                ));
-              } else {
-                //TODO: remove handler
-              }
-            },
-          ),
-        ),
-      ),
+                );
+              },
+            ),
     );
   }
 }
